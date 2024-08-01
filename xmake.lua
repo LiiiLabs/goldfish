14cec4ab07b6b8a37f6cd6b664c150e45c4678fd
Darcy Shen <da@liii.pro>
2024-08-02 00:00:32 +0800

    wip

--- xmake.lua
+++ xmake.lua
@@ -18,3 +18,37 @@ target ("goldfish") do
     add_files ("src/goldfish.cpp")
     add_packages("s7")
 end
+
+function add_scheme_test(filepath)
+    local testname = path.basename(filepath)
+    target(testname) do
+        set_enabled(not is_plat("wasm"))
+        set_kind("phony")
+        set_group("tests")
+        add_deps("goldfish")
+        on_run(function (target)
+            name = target:name()
+            print("------------------------------------------------------")
+            print("Executing: " .. name)
+            params = {
+              file_path
+            }
+            if is_plat("macosx", "linux") then
+                binary = target:deps()["goldfish"]:targetfile()
+            else
+                print("Unsupported plat $(plat)")
+            end
+            cmd = binary
+            print("cmd: " .. cmd)
+            print("filepath: " .. filepath)
+            os.execv(cmd, params)
+        end)
+    end
+end
+
+
+all_tests = os.files("tests/**/*-test.scm")
+
+for _, filepath in ipairs(all_tests) do
+    add_scheme_test(filepath)
+end
