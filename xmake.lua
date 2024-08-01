11f6504ef5b475ae07ce2e9e9ea12ff370373bdb
Darcy Shen <da@liii.pro>
2024-08-02 00:04:14 +0800

    wip

--- xmake.lua
+++ xmake.lua
@@ -5,8 +5,10 @@ add_rules("mode.releasedbg", "mode.release", "mode.debug", "mode.profile")
 -- plat
 set_allowedplats("linux", "macosx", "windows")
 
+-- proj
 set_project("Goldfish Scheme")
 
+-- repo
 add_repositories("goldfish-repo xmake")
 
 S7_VERSION = "20240702"
@@ -17,38 +19,4 @@ target ("goldfish") do
     set_targetdir("$(projectdir)/bin/")
     add_files ("src/goldfish.cpp")
     add_packages("s7")
-end
-
-function add_scheme_test(filepath)
-    local testname = path.basename(filepath)
-    target(testname) do
-        set_enabled(not is_plat("wasm"))
-        set_kind("phony")
-        set_group("tests")
-        add_deps("goldfish")
-        on_run(function (target)
-            name = target:name()
-            print("------------------------------------------------------")
-            print("Executing: " .. name)
-            params = {
-              file_path
-            }
-            if is_plat("macosx", "linux") then
-                binary = target:deps()["goldfish"]:targetfile()
-            else
-                print("Unsupported plat $(plat)")
-            end
-            cmd = binary
-            print("cmd: " .. cmd)
-            print("filepath: " .. filepath)
-            os.execv(cmd, params)
-        end)
-    end
-end
-
-
-all_tests = os.files("tests/**/*-test.scm")
-
-for _, filepath in ipairs(all_tests) do
-    add_scheme_test(filepath)
 end
