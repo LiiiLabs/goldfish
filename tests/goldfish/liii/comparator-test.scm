44e6af7b406adaa39c7b445d1f58562770016f6b
Darcy Shen <da@liii.pro>
2024-09-05 20:10:10 +0800

    wip

--- tests/goldfish/liii/comparator-test.scm
+++ tests/goldfish/liii/comparator-test.scm
@@ -3,10 +3,21 @@
 
 (check-set-mode! 'report-failed)
 
-(check (boolean<? #t #t) => #f)
-(check (boolean<? #f #f) => #f)
-(check (boolean<? #f #t) => #t)
-(check (boolean<? #t #f) => #f)
+(define boolean-comparator
+  (make-comparator boolean? eq? boolean<? default-hash))
+
+(check-false (boolean<? #t #t))
+(check-false (<? boolean-comparator #t #t))
+(check-false (boolean<? #f #f))
+(check-false (<? boolean-comparator #f #f))
+(check-true (boolean<? #f #t))
+(check-true (<? boolean-comparator #f #t))
+(check-false (boolean<? #t #f))
+(check-false (<? boolean-comparator #t #f))
+
+(check-false (complex<? 1 1))
+(check-true (complex<? 1i 2i))
+(check-true (complex<? 1+2i 2+2i))
 
 (check (> (boolean-hash #t) 0) => #t)
 (check (boolean-hash #f) => 0)
