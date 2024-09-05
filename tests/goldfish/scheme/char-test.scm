f2b6843282b1ca76a2ee7371dbba578ff8602dfd
Darcy Shen <da@liii.pro>
2024-09-05 18:23:45 +0800

    Open source the literated (scheme file) and (scheme char)

--- tests/goldfish/scheme/char-test.scm
+++ tests/goldfish/scheme/char-test.scm
@@ -1,4 +1,20 @@
-(import (srfi srfi-78))
+;
+; Copyright (C) 2024 The Goldfish Scheme Authors
+;
+; Licensed under the Apache License, Version 2.0 (the "License");
+; you may not use this file except in compliance with the License.
+; You may obtain a copy of the License at
+;
+; http://www.apache.org/licenses/LICENSE-2.0
+;
+; Unless required by applicable law or agreed to in writing, software
+; distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
+; WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
+; License for the specific language governing permissions and limitations
+; under the License.
+;
+
+(import (liii check))
 
 (check-set-mode! 'report-failed)
 
@@ -19,5 +35,4 @@
 (check (char-upcase #\null) => #\null)
 
 (check-report)
-(if (check-failed?) (exit -1))
 
