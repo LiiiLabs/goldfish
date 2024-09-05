1169c30a6a4bebdfd244f727fce5cb4f982858b0
Darcy Shen <da@liii.pro>
2024-09-05 10:10:42 +0800

    Open source the literated (liii vector) (#41)

---
+++ tests/goldfish/liii/vector-test.scm
@@ -0,0 +1,80 @@
+;
+; Copyright (C) 2024 The Goldfish Scheme Authors
+;
+; Licensed under the Apache License, Version 2.0 (the "License");
+; you may not use this file except in compliance with the License.
+; You may obtain a copy of the License at
+;
+; http://www.apache.org/licenses/LICENSE-2.0
+;
+; Unless required by applicable law or agreed to in writing, software
+; distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
+; WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
+; License for the specific language governing permissions and limitations
+; under the License.
+;
+
+(import (liii list)
+        (liii check)
+        (liii vector)
+        (scheme base))
+
+(check-set-mode! 'report-failed)
+
+(for-each (lambda (p) (check (procedure? p) => #t))
+  (list
+   vector-empty?
+   vector-count
+   vector-any vector-every vector-copy vector-copy!
+   vector-index vector-index-right vector-partition
+   vector-swap!))
+
+(check-true (vector-empty? (vector)))
+(check-false (vector-empty? (vector 1)))
+(check-catch 'type-error (vector-empty? 1))
+
+(check (vector-count even? #()) => 0)
+(check (vector-count even? #(1 3 5 7 9)) => 0)
+(check (vector-count even? #(1 3 4 7 8)) => 2)
+
+(check (vector-any even? #()) => #f)
+(check (vector-any even? #(1 3 5 7 9)) => #f)
+(check (vector-any even? #(1 3 4 7 8)) => #t)
+
+(check (vector-every odd? #()) => #t)
+(check (vector-every odd? #(1 3 5 7 9)) => #t)
+(check (vector-every odd? #(1 3 4 7 8)) => #f)
+
+(check (vector-index even? #()) => #f)
+(check (vector-index even? #(1 3 5 7 9)) => #f)
+(check (vector-index even? #(1 3 4 7 8)) => 2)
+
+(check (vector-index-right even? #()) => #f)
+(check (vector-index-right even? #(1 3 5 7 9)) => #f)
+(check (vector-index-right even? #(1 3 4 7 8)) => 4)
+
+(define (vector-partition->list pred v)
+  (let-values (((ret cnt) (vector-partition pred v))) (list ret cnt)))
+
+(check (vector-partition->list even? #()) => '(#() 0))
+(check (vector-partition->list even? #(1 3 5 7 9)) => '(#(1 3 5 7 9) 0))
+(check (vector-partition->list even? #(1 3 4 7 8)) => '(#(4 8 1 3 7) 2))
+
+(define my-vector (vector 0 1 2 3))
+(vector-swap! my-vector 1 2)
+(check my-vector => #(0 2 1 3))
+
+(define my-vector (vector 0 1 2 3))
+(vector-swap! my-vector 1 1)
+(check my-vector => #(0 1 2 3))
+
+(define my-vector (vector 0 1 2 3))
+(vector-swap! my-vector 0 (- (vector-length my-vector) 1))
+(check my-vector => #(3 1 2 0))
+
+(check-catch
+ 'out-of-range
+ (vector-swap! my-vector 1 (vector-length my-vector)))
+
+(check-report)
+
