596730c1235f480d4986057ab8c578ef5f1f194c
Darcy Shen <da@liii.pro>
2024-09-05 10:07:04 +0800

    wip

--- tests/goldfish/liii/vector-test.scm
+++ tests/goldfish/liii/vector-test.scm
@@ -1,10 +1,23 @@
-; Copyright (c) 2024 Da, Nian @ Liii Network Inc.
-; All right reserved.
+;
+; Copyright (C) 2024 The Goldfish Scheme Authors
+;
+; Licensed under the Apache License, Version 2.0 (the "License");
+; you may not use this file except in compliance with the License.
+; You may obtain a copy of the License at
+;
+; http://www.apache.org/licenses/LICENSE-2.0
+;
+; Unless required by applicable law or agreed to in writing, software
+; distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
+; WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
+; License for the specific language governing permissions and limitations
+; under the License.
+;
 
-(import (scheme base)
-        (srfi srfi-1)
+(import (liii list)
         (liii check)
-        (srfi srfi-133))
+        (liii vector)
+        (scheme base))
 
 (check-set-mode! 'report-failed)
 
