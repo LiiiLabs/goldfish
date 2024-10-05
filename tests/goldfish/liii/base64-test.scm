2721fab3eb56bf3138409f8d90c20b4bc5584499
Darcy Shen <da@liii.pro>
2024-10-05 22:32:16 +0800

    wip

--- tests/goldfish/liii/base64-test.scm
+++ tests/goldfish/liii/base64-test.scm
@@ -1,7 +1,15 @@
 (import (liii check)
         (liii base64))
 
-(check (utf8->string (base64-encode (string->utf8 "a"))) => "YQ==")
+(check (string-base64-encode "") => "")
+(check (string-base64-encode "a") => "YQ==")
+(check (string-base64-encode "z") => "eg==")
+(check (string-base64-encode "f") => "Zg==")
+(check (string-base64-encode "fo") => "Zm8=")
+(check (string-base64-encode "foo") => "Zm9v")
+(check (string-base64-encode "foob") => "Zm9vYg==")
+(check (string-base64-encode "fooba") => "Zm9vYmE=")
+(check (string-base64-encode "foobar") => "Zm9vYmFy")
 
 (check-report)
 
