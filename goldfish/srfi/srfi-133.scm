a7a36af13743b25be03190e3adf1f99fff4b84dc
Duolei-Wang <wangdl2020@mail.sustech.edu.cn>
2024-11-02 10:51:00 +0800

    improve the vector=

--- goldfish/srfi/srfi-133.scm
+++ goldfish/srfi/srfi-133.scm
@@ -30,31 +30,25 @@
     (error 'type-error "v is not a vector"))
   (zero? (vector-length v)))
 
-; (vector= elt=? vec ...) -> boolean, srfi-133 support. 
-; Use the given element comparators to compare the input vectors.
-; When the input is null or just one vector, return #t.
-; Else, it will compare the vectors in the argument list one by one.
-;     If all the vectors are equal under the elt's comparison, return #t, else #f.
-(define (vector= elt . rest)
-  ; The inner method provides type-check and two vectors comparison.
+(define (vector= elt=? . rest)
   (define compare2vecs
     (typed-lambda ((cmp procedure?) (vec1 vector?) (vec2 vector?))
-      (let*  ((len1 (vector-length vec1))
-              (len2 (vector-length vec2)))
-        (if (not (= len1 len2)) #f
-            (let loop ((ilhs 0) (irhs 0) (imax len1))
-                 (if (= ilhs imax) #t
-                     (if (not (cmp (vec1 ilhs) (vec2 irhs))) #f
-                         (loop (+ 1 ilhs) (+ 1 irhs) imax))))))))
-  (if (not (procedure? elt)) (error 'type-error "elt should be a procedure")
-      (if (or(null? rest) (= 1 (length rest))) #t
-          (let loop ((vec1 (car rest))
-                     (vec2 (car (cdr rest)))
-                     (vrest (cdr (cdr rest))))
-               (if (compare2vecs elt vec1 vec2)
-                   (if (null? vrest) #t
-                       (loop vec2 (car vrest) (cdr vrest)))
-                   #f)))))
+       (let*  ((len1 (vector-length vec1))
+               (len2 (vector-length vec2)))
+         (if (not (= len1 len2)) #f
+             (let loop ((ilhs 0) (irhs 0) (len len1))
+                  (if (= ilhs len) #t
+                      (if (not (cmp (vec1 ilhs) (vec2 irhs))) #f 
+                          (loop (+ 1 ilhs) (+ 1 irhs) len))))))))
+  (when (not (procedure? elt=?)) (error 'type-error "elt=? should be a procedure"))
+  (if (or(null? rest) (= 1 (length rest))) #t
+      (let loop ((vec1 (car rest)) (vec2 (car (cdr rest))) (vrest (cdr (cdr rest))))
+        (let1 rst (compare2vecs elt=? vec1 vec2)
+           (when (not (boolean? rst)) (error 'type-error "elt=> should return bool"))
+           (if (compare2vecs elt=? vec1 vec2)
+               (if (null? vrest) #t
+                   (loop vec2 (car vrest) (cdr vrest)))
+               #f)))))
 ; TODO optional parameters
 (define (vector-count pred v)
   (let loop ((i 0) (count 0))
