5a5f8d6f3e0b80ea3f9ec81babfb7a9ca538ffe3
Darcy Shen <da@liii.pro>
2024-11-19 17:43:24 +0800

    wip

--- goldfish/liii/argparse.scm
+++ goldfish/liii/argparse.scm
@@ -1,12 +1,12 @@
 (define-library (liii argparse)
-  (import (liii base)
-          (liii error)
-          (liii list)
-          (liii hash-table))
-  
-  (export make-parser)
+(import (liii base)
+        (liii error)
+        (liii list)
+        (liii hash-table))
 
-  (begin
+(export make-parser)
+
+(begin
     ;; Internal argument record structure
     ;; (name type short-name default current-value)
     (define (make-arg-record name type short-name default)
@@ -31,74 +31,77 @@
              value
              (error "Value is not a string")))
         (else (error "Unsupported type" type))))
-    
-    ;; Main parser constructor
-    (define (make-parser)
-      (let ((args-table (make-hash-table)))
-        (lambda (command . args)
-          (case command
-            ;; Add new argument
-            ((add-argument)
-             (let* ((name (car args))
-                    (type (cadr args))
-                    (options (caddr args))
-                    (short-name (get-option options 'short))
-                    (default (get-option options 'default))
-                    (arg-record (make-arg-record name type short-name default)))
-               (unless (memq type '(number string))
-                 (error "Type must be either 'number or 'string" type))
-               (hash-table-set! args-table name arg-record)
-               (when short-name
-                 (hash-table-set! args-table short-name arg-record))))
-            
-            ;; Parse command line arguments
-            ((parse-args)
-             (let loop ((args (car args))
-                       (current-arg #f))
-               (if (null? args)
-                   args-table
-                   (let ((arg (car args)))
-                     (cond
-                       ;; Long form --name
-                       ((and (string? arg) 
-                             (>= (string-length arg) 3)
-                             (string=? (substring arg 0 2) "--"))
-                        (let* ((name (substring arg 2))
-                               (found (hash-table-ref args-table name #f)))
-                          (if found
-                              (if (null? (cdr args))
-                                  (error "Missing value for argument" name)
-                                  (begin
-                                    (let ((value (convert-value (cadr args) (cadr found))))
-                                      (set-car! (cddddr found) value))
-                                    (loop (cddr args) #f)))
-                              (error "Unknown argument" name))))
-                       
-                       ;; Short form -n
-                       ((and (string? arg)
-                             (>= (string-length arg) 2)
-                             (char=? (string-ref arg 0) #\-))
-                        (let* ((name (substring arg 1))
-                               (found (hash-table-ref args-table name #f)))
-                          (if found
-                              (if (null? (cdr args))
-                                  (error "Missing value for argument" name)
-                                  (begin
-                                    (let ((value (convert-value (cadr args) (cadr found))))
-                                      (set-car! (cddddr found) value))
-                                    (loop (cddr args) #f)))
-                              (error "Unknown argument" name))))
-                       
-                       (else (loop (cdr args) current-arg)))))))
-            
-            ;; Get argument value
-            ((get-argument)
-             (let ((found (hash-table-ref args-table (car args) #f)))
+
+(define (%add-argument args-ht args)
+  (display* "add-argument: " args "\n")
+  (let* ((name (car args))
+         (type (cadr args))
+         (options (caddr args))
+         (short-name (get-option options 'short))
+         (default (get-option options 'default))
+         (arg-record (make-arg-record name type short-name default)))
+    (unless (memq type '(number string))
+             (error "Type must be either 'number or 'string" type))
+    (hash-table-set! args-ht name arg-record)
+    (when short-name
+          (hash-table-set! args-ht short-name arg-record))))
+
+(define (%get-argument args-ht args)
+  (display* "args: " args "\n")
+  (let ((found (hash-table-ref/default args-ht (car args) #f)))
+    (if found
+        (car (cddddr found))
+        (error "Argument not found" (car args)))))
+
+(define (%parse-args args-ht args)
+  (display* "parse: " args "\n")
+  (let loop ((args (car args))
+             (current-arg #f))
+    (if (null? args)
+        args-ht
+        (let ((arg (car args)))
+          (cond
+            ;; Long form --name
+            ((and (string? arg) 
+                  (>= (string-length arg) 3)
+                  (string=? (substring arg 0 2) "--"))
+             (let* ((name (substring arg 2))
+                    (found (hash-table-ref args-ht name #f)))
                (if found
-                   (car (cddddr found))
-                   (error "Argument not found" (car args)))))
+                   (if (null? (cdr args))
+                       (error "Missing value for argument" name)
+                       (begin
+                         (let ((value (convert-value (cadr args) (cadr found))))
+                           (set-car! (cddddr found) value))
+                         (loop (cddr args) #f)))
+                   (error "Unknown argument" name))))
             
-            (else (error "Unknown parser command" command)))))))
+            ;; Short form -n
+            ((and (string? arg)
+                  (>= (string-length arg) 2)
+                  (char=? (string-ref arg 0) #\-))
+             (let* ((name (substring arg 1))
+                    (found (hash-table-ref args-ht name #f)))
+               (if found
+                   (if (null? (cdr args))
+                       (error "Missing value for argument" name)
+                       (begin
+                         (let ((value (convert-value (cadr args) (cadr found))))
+                           (set-car! (cddddr found) value))
+                         (loop (cddr args) #f)))
+                   (error "Unknown argument" name))))
+            
+            (else (loop (cdr args) current-arg)))))))
+
+;; Main parser constructor
+(define (make-parser)
+  (let ((args-ht (make-hash-table)))
+    (lambda (command . args)
+      (case command
+        ((add-argument) (%add-argument args-ht args))
+        ((get-argument) (%get-argument args-ht args))
+        ((parse-args) (%parse-args args-ht args))
+        (else (error "Unknown parser command" command)))))))
 
   ;; End library definition
   )
