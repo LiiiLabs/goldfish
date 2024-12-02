207058b554f6a04df7f5ef42435c43def2073cfc
Ancker-0 <andy87654@outlook.com>
2024-12-02 13:18:01 +0800

    Abandon some implementations which violates SRFI 132

--- goldfish/srfi/srfi-132.scm
+++ goldfish/srfi/srfi-132.scm
@@ -15,9 +15,11 @@
 ;
 
 (define-library (srfi srfi-132)
-(export list-merge  list-sort  list-stable-sort  vector-merge  vector-sort  vector-stable-sort
+(export list-sorted? vector-sorted?
+        list-merge  list-sort  list-stable-sort  vector-merge  vector-sort  vector-stable-sort
         list-merge! list-sort! list-stable-sort! vector-merge! vector-sort! vector-stable-sort!)
 (import (liii list)
+        (liii error)
         (scheme case-lambda))
 (begin
 
@@ -40,6 +42,23 @@
                        (format () "~a\n=> ~a\n" (list ,f-name ,@f-args) res)
                        (apply values res)))))
 
+  (define (list-sorted? less-p lis)
+    (if (null? lis)
+      #t
+      (do ((first lis (cdr first))
+           (second (cdr lis) (cdr second))
+           (res #t (not (less-p (car second) (car first)))))
+        ((or (null? second) (not res)) res))))
+
+  ; TODO optional parameters
+  (define (vector-sorted? less-p v)
+    (let ((start 0)
+          (end (length v)))
+      (do ((first start (+ 1 first))
+           (second (+ 1 start) (+ 1 second))
+           (res #t (not (less-p (vector-ref v second) (vector-ref v first)))))
+        ((or (>= second end) (not res)) res))))
+
   (define (list-merge less-p lis1 lis2)
     (let loop
       ((res '())
@@ -51,6 +70,9 @@
         ((null? lis2) (loop (cons (car lis1) res) lis2 (cdr lis1)))
         ((less-p (car lis2) (car lis1)) (loop (cons (car lis2) res) lis1 (cdr lis2)))
         (else (loop (cons (car lis1) res) (cdr lis1) lis2)))))
+
+  ; this list-merge! violates SRFI 132, since it does not satisfy the constant running space constraint specified in SRFI 132, and does not work "in place"
+  (define list-merge! list-merge)
 
   (define (list-stable-sort less-p lis)
     (define (sort l r)
@@ -65,6 +87,22 @@
     (sort 0 (length lis)))
 
   (define list-sort list-stable-sort)
+  (define list-sort! list-stable-sort)
+  (define list-stable-sort! list-stable-sort)
+
+  (define vector-stable-sort
+    (case-lambda
+      ((less-p v)
+       (list->vector (list-stable-sort less-p (vector->list v))))
+      ((less-p v start)
+       (list->vector (list-stable-sort less-p (subvector->list v start (vector-length v)))))
+      ((less-p v start end)
+       (list->vector (list-stable-sort less-p (subvector->list v start end))))))
+
+  (define vector-sort vector-stable-sort)
+
+  (define (vector-sort! . r) (???))
+  (define (vector-stable-sort! . r) (???))
 
   (define (subvector->list v start end)
     (do ((r '() (cons (vector-ref v p) r))
@@ -84,25 +122,7 @@
       ((less-p v1 v2 start1 end1 start2 end2)
        (list->vector (list-merge less-p (subvector->list v1 start1 end1) (subvector->list v2 start2 end2))))))
 
-  (define vector-stable-sort
-    (case-lambda
-      ((less-p v)
-       (list->vector (list-stable-sort less-p (vector->list v))))
-      ((less-p v start)
-       (list->vector (list-stable-sort less-p (subvector->list v start (vector-length v)))))
-      ((less-p v start end)
-       (list->vector (list-stable-sort less-p (subvector->list v start end))))))
-
-  (define vector-sort vector-stable-sort)
-
-  (define-macro (define/! x)
-    (when (not (symbol? x)) (error 'syntax-error))
-    ;(format () "~a " (string->symbol (string-append (symbol->string x) "_")))
-    `(define ,(string->symbol (string-append (symbol->string x) "!")) ,x))
-  (define-macro (define-more/! . xs)
-    `(begin ,@(map (lambda (x) `(define/! ,x)) xs)))
-
-  (define-more/! list-merge list-sort list-stable-sort vector-merge vector-sort vector-stable-sort)
+  (define (vector-merge! . r) (???))
 
 ) ; end of begin
 ) ; end of library
