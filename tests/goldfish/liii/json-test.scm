376e93c6764dca347719ad5912f1c0961272cdd8
Darcy Shen <da@liii.pro>
2025-01-02 15:45:29 +0800

    json-set and json-set* in (liii json)

--- tests/goldfish/liii/json-test.scm
+++ tests/goldfish/liii/json-test.scm
@@ -18,6 +18,8 @@
         (liii json))
 
 (check (string->json "{\"age\":18}") => `(("age" . 18)))
+(check (string->json "{age:18}") => `((age . 18)))
+
 (check (string->json "{\"name\":\"中文\"}") => `(("name" . "中文"))) 
 
 (check (json->string '(("age" . 18))) => "{\"age\":18}")
@@ -27,6 +29,52 @@
 (check (json-ref #(0 1 2 3) 0) => 0)
 
 (check (json-ref* `((bob . ((age . 18) (sex . male)))) 'bob 'age) => 18)
+
+(let* ((j0 `((age . 18) (sex . male)))
+       (j1 (json-set j0 'age 19))
+       (j2 (json-set j0 'age 'null)))
+      (check (json-ref j0 'age) => 18)
+      (check (json-ref j1 'age) => 19)
+      (check (json-ref j2 'age) => '()))
+
+(let1 j `(("age" . 18) ("sex" . male))
+      (check (json-ref (json-set j "age" 19) "age") => 19)
+      (check (json-ref j "age") => 18))
+
+(let* ((j0 #(red green blue))
+       (j1 (json-set j0 0 'black)))
+  (check j0 => #(red green blue))
+  (check j1 => #(black green blue)))
+
+(let* ((j0 `((bob . 18) (jack . 16)))
+       (j1 (json-set j0 #t 3))
+       (j2 (json-set j0 #t (lambda (x) (+ x 1)))))
+  (check j1 => `((bob . 3) (jack . 3)))
+  (check j2 => `((bob . 19) (jack . 17))))
+
+(let* ((json '((name . "Alice") (age . 25)))
+       (updated-json (json-set* json 'age 26)))
+  (check (json-ref updated-json 'age) => 26))
+
+(let* ((json '((person . ((name . "Alice")
+                          (age . 25)))))
+       (updated-json (json-set* json 'person 'age 26)))
+  (check (json-ref* updated-json 'person 'age) => 26))
+
+(let* ((json '((person . ((name . "Alice")
+                          (age . 25)
+                          (address . ((city . "Wonderland")
+                                      (zip . "12345")))))))
+       (updated-json (json-set* json 'person 'address 'city "Newland")))
+  (check (json-ref* updated-json 'person 'address 'city) => "Newland"))
+
+(let* ((json '((name . "Alice") (age . 25)))
+       (updated-json (json-set* json 'age (lambda (x) (+ x 1)))))
+  (check (json-ref updated-json 'age) => 26))
+
+(let* ((json '((person . ((name . "Alice") (age . 25)))))
+       (updated-json (json-set* json 'person 'age (lambda (x) (+ x 1)))))
+  (check (json-ref* updated-json 'person 'age) => 26))
 
 (check-report)
 
