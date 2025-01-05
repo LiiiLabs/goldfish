b84351353402c9d5fe046bcbb23beb94f73e1232
Darcy Shen <da@liii.pro>
2025-01-06 00:14:12 +0800

    wip

--- http/tests/chat_demo.scm
+++ http/tests/chat_demo.scm
@@ -35,7 +35,7 @@
 
 (define payload
   `(
-    ("model" . "Qwen/Qwen2.5-Coder-7B-Instruct")
+    ("model" . "deepseek-ai/DeepSeek-V2.5")
     ("messages" . #())
     ("max_tokens" . 512)
    )
@@ -50,26 +50,31 @@
         (r 'status-code))))
 
 (define questions
-  #("唐宋八大家是哪八位"
+  #("唐宋八大家是哪八位（简短回答）"
     "请按照顺序返回上一个回答中的第五位"
-    "上一个问题的回答一共几个汉字？"
-    "介绍他的生平和作品"))
+    "用双引号引用上一个问题的回答，并告诉我一共多少个汉字？"
+    "介绍他的生平和作品（简短回答）"))
 
-(define (message text)
-  (let1 msg `(("role" . "user") ("content" . ""))
-        (json-set msg "content" text)))
+(define (message role content)
+  `(("role" . ,role) ("content" . ,content)))
+
+(define (append-message q msg)
+  (json-set q "messages" (lambda (v) (vector-append v (vector msg)))))
 
 (let loop ((i 0) (payload payload) (tokens 0))
   (if (< i (length questions))
-      (let* ((q (json-push* payload "messages" i (message (questions i))))
+      (let* ((q (append-message payload (message "user" (questions i))))
              (r (chat q))
              (j (string->json r))
              (a (json-ref* j "choices" 0 "message" "content")))
           (display* "Q: " (questions i) "\n")
           (display* "A: " a "\n")
           (newline)
+          ; (display* "发送给服务端的JSON：\n" (json->string q))
+          (newline)
+          (newline)
           (loop (+ i 1)
-                (json-set* q "messages" i "content" (lambda (y) (string-append y "\n回答：" a)))
+                (append-message q (message "assistant" a))
                 (+ tokens (json-ref* j "usage" "total_tokens"))))
-      (display* "Total tokens: " tokens "\n")))
+      (display* "Total tokens: " tokens "\n" payload)))
 
