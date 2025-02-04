a5322a950f23f19a516de3277f04c6a87c35da5b
jackyansongli <yli340@uic.edu>
2025-02-04 14:15:44 +0800

    revise based on Da's comments

--- goldfish/liii/lang.scm
+++ goldfish/liii/lang.scm
@@ -424,40 +424,34 @@
     (if (null? xs)                                 
         result
         (apply result xs)))) 
-(define (%split sep) 
-  (rich-vector (list->vector (string-split data sep))))
-)
 
-(define (string-split str sep)
-  (let ((sep-len (string-length sep))
-        (str-len (string-length str)))
-    ;; Helper function to compare substrings
-    (define (substring= str1 start1 str2 start2 len)
-      (let loop ((i 0))
-        (cond
-         ((= i len) #t)
-         ((char=? (string-ref str1 (+ start1 i)) 
-                 (string-ref str2 (+ start2 i)))
-          (loop (+ i 1)))
-         (else #f))))
-    
-    (cond
-     ((= sep-len 0) (list str))  ;; Handle empty separator
-     (else
-      (let loop ((start 0) (result '()))
-        (if (>= start str-len)
-            (reverse result)
-            (let ((found-at #f))
-              ;; Manual search for the separator
-              (do ((i start (+ i 1)))
-                  ((or found-at (> i (- str-len sep-len))))
-                (when (and (<= (+ i sep-len) str-len)
-                           (substring= str i sep 0 sep-len))
-                  (set! found-at i)))
-              (if found-at
-                  (loop (+ found-at sep-len)
-                        (cons (substring str start found-at) result))
-                  (reverse (cons (substring str start str-len) result))))))))))
+(define (%split sep)
+  ;; 在 %split 内部定义 string-split 作为子函数
+  (define (string-split str sep)
+    (let ((sep-len (string-length sep))
+          (str-len (string-length str)))
+      (cond
+       ;; 处理分隔符长度为0的特殊情况
+       ((= sep-len 0)
+        (map string (string->list str)))
+       (else
+        (let loop ((start 0) (result '()))
+          (if (>= start str-len)
+              (reverse result)
+              (let ((found-at
+                     (let search ((i start))
+                       (and (<= (+ i sep-len) str-len)
+                            (or (and (string-starts? (substring str i (+ i sep-len)) sep)
+                                     i)
+                                (search (+ i 1)))))))
+                (if found-at
+                    (loop (+ found-at sep-len)
+                          (cons (substring str start found-at) result))
+                    (reverse (cons (substring str start str-len) result))))))))))
+  ;; 利用(liii string)定义的 string-split 来处理 data
+  (rich-vector (list->vector (string-split data sep))))
+
+)
 
 (define-case-class rich-list ((data list?))
 
